047439827490c5e7e9d19df7ff64dae1081e62fc
Livni <llmc0383@gmail.com>
2023-05-16 15:05:56 -0600

    CONEXION AL SERVIDOR local

--- conexion.php
+++ conexion.php
@@ -4,6 +4,8 @@ try {
     $conexion= new PDO ("informix:host=host.docker.internal; service=9088;database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix","in4mix");
     echo "conexion exitosa";
 } catch (PDOException $e){
+    echo "CONEXION A LA BASE DE DATOS MARTINEZ_C";
+    echo "<br>";
     echo $e->getMessage ();
 }
 echo "<br>";
